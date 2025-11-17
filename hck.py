5c1a645e7a4c014153f0250f50dce1cbfd99b92b
Prabhat Shukla <prabhatshukla0849@gmail.com>
2025-11-17 21:09:50 +0530

    py sdk

---
+++ hck.py
@@ -0,0 +1,21 @@
+
+import sys
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        greet(sys.argv[1])
+    else:
+        print("Usage: python $PYTHON_SCRIPT <name>")
+EOF
+
+# Make the Python script executable
+chmod +x $PYTHON_SCRIPT
+
+# Run the Python script using bash to execute it
+./$PYTHON_SCRIPT World
+
+# Clean up the created script file
+rm $PYTHON_SCRIPT
